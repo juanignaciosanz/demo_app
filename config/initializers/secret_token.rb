3c956edc5bfe206ba714fcae2834d2397efd63a4
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2013-02-02 20:22:50 +0100

    Initial commit
    Added production environmento with gem postgres for Heroku deployment
    stage. Installed bundled after update without production option

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd6df87650bcc9989352632ad0b6c14d53197831d816c99aee87a4593e4d6ea25a7ffeafe18a3a56ff2c4e35b31bef0cdaa8c375d7132c722ffd842b7f302156d'
